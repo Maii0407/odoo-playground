4ab20cb7fa71191dd546b1868f101eb9fddd8bed
akmal-izuddin <87862368+Maii0407@users.noreply.github.com>
2023-12-19 10:42:59 +0800

    chore: create test module/application

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name': 'estate',
+  'version': '0.1',
+  'author': 'Bob the Builder',
+  'category': 'Test',
+  'summary': 'test module',
+  'description': 'description',
+  'website': 'https://github.com/Maii0407/odoo-playground',
+  'installable': True,
+  'application': True,
+  'auto_install': False
+}
